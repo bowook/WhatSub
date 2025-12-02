56d6cdaf6834dea5e741fe7aa59fa208afec9687
bowook <mbosx316@naver.com>
2025-12-02 14:04:19 +0900

    chore: 초기 설정

---
+++ whatsub-api/src/main/java/com/whatsub/WhatsubApiApplication.java
@@ -0,0 +1,13 @@
+package com.whatsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsubApiApplication.class, args);
+	}
+
+}
