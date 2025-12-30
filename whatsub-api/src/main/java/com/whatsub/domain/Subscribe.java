ef119dfca8be9b385255d3f05362210217f3cc1f
Leehogyu <59430391+JamongFriend@users.noreply.github.com>
2025-12-30 19:40:30 +0900

    refactor: NoArgsConstructor (access = AccessLevel.PROTECTED)를 추가

--- whatsub-api/src/main/java/com/whatsub/domain/Subscribe.java
+++ whatsub-api/src/main/java/com/whatsub/domain/Subscribe.java
@@ -1,13 +1,16 @@
 package com.whatsub.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Subscribe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
